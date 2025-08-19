9e41bd3c84802c9c2e58b7bf00cc7728eb32a2d2
SylviaRemington <sylviaremington@gmail.com>
2025-08-18 19:56:03 -0700

    notation about authorization

--- src/services/hootService.js
+++ src/services/hootService.js
@@ -1,9 +1,33 @@
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/hoots`;
 
-
 /*
 If you completed the setup steps, your .env should contain 
 a VITE_BACK_END_SERVER_URL environment variable set to 
 http://localhost:3000. When running our app locally, the 
 BASE_URL will read as http://localhost:3000/hoots.
+*/
+
+// src/services/hootService.js
+
+const index = async () => {
+    try {
+        const res = await fetch(BASE_URL, {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+        });
+        return res.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export {
+    index,
+};
+
+/*
+If you look at the controllers/hoots.js file in your back-end application,
+ you’ll notice that all of our routes for hoots are protected by the verifyToken 
+ middleware.
+
+As a result, all of our hoot service functions will require this Authorization header.
 */
