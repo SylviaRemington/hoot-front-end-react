3acca34fc9a3e692474ea1c1db2e7cc4e7cf57b1
SylviaRemington <sylviaremington@gmail.com>
2025-08-18 19:12:54 -0700

    notes on building the hootlist component

--- src/components/HootList/HootList.jsx
+++ src/components/HootList/HootList.jsx
@@ -6,3 +6,25 @@ const HootList = (props) => {
 
 export default HootList;
 
+
+
+/*
+Notes On Building the HootList Component --
+In this lesson, we’ll implement the following user story:
+- As a user, I should be able to see a list of all hoots on a ‘List’ page.
+
+Let’s walk through some of the logic involved here:
+- Our app will store the hoots state in the App component. 
+We’ll pass this state down to the new HootList component.
+
+- Within the HootList component, we’ll map() over hoots to produce 
+an array of <article> tags. Each <article> tag will render a single hoot 
+object.
+
+- The data held in hoots state will originate in our back-end app. 
+We’ll use the Fetch API to retrieve this data and use it in the front-end
+ app. We’ll group these fetch() requests in a dedicated module for each 
+ resource in our application. These modules are commonly referred to as 
+ services.
+
+*/
