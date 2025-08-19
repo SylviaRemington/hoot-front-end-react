d5c68ff40f77963b3b145d7aa8b89456b5d8153e
SylviaRemington <sylviaremington@gmail.com>
2025-08-18 19:44:18 -0700

    adding notes regarding back-end-server-url and base-url

--- src/services/hootService.js
+++ src/services/hootService.js
@@ -1,1 +1,9 @@
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/hoots`;
+
+
+/*
+If you completed the setup steps, your .env should contain 
+a VITE_BACK_END_SERVER_URL environment variable set to 
+http://localhost:3000. When running our app locally, the 
+BASE_URL will read as http://localhost:3000/hoots.
+*/
